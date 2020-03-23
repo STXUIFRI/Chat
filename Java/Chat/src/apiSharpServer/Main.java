28353e4bfbbc1f604d665303b20d10a42d3f5c82
elach <el.ach@gmx.de>
2020-03-23 19:45:08 +0100

    Refactor (Currently not Working)

--- Java/Chat/src/apiSharpServer/Main.java
+++ Java/Chat/src/apiSharpServer/Main.java
@@ -1,27 +1,19 @@
 package apiSharpServer;
 
 import SqlConnection.Connection;
-import apiSharpServer.data.Chat;
-import apiSharpServer.data.Data;
-import apiSharpServer.data.Login;
-import apiSharpServer.data.Message;
-import org.json.JSONObject;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.*;
 
 public class Main {
 
-    private List<Thread> clients = new LinkedList<>();
+    private Map<String, Client> clients = new HashMap<>();
     private Connection connectionDB = new Connection();
     private Security security = new Security();
+
+
 
 
     public Main() {
@@ -34,11 +26,12 @@ public class Main {
             while (true) {
                 System.out.println("Waiting for Client...");
                 // wait.start();
+                startThread();
                 Socket client = server.accept();
                 //wait.stop();
                 System.out.printf("%nClient Connected: %s%n", client.getLocalAddress());
 
-                newClientThread(client);
+                newClient(client);
             }
 
         } catch (IOException e) {
@@ -46,125 +39,36 @@ public class Main {
         }
     }
 
-    private void newClientThread(Socket client) {
-        Thread clientThread = new Thread(() -> {
-            try {
-                DataInputStream in = new DataInputStream(client.getInputStream());
-                DataOutputStream out = new DataOutputStream(client.getOutputStream());
-
-                Data connected = new Data(ActionEnum.CONNECTED.getI(), null, null, null);
-
-                out.write(connected.packToJson().toString().getBytes());
-
-                listener(client, in, out);
-                return;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        clientThread.start();
-
-        clients.add(clientThread);
+    private void newClient(Socket client) {
+        String id = genID();
+        Client c = new Client(client,connectionDB,security);
+        c.setTokenName(id);
+        clients.put(id,c);
     }
 
-    private void listener(Socket client, DataInputStream in, DataOutputStream out) throws IOException, InterruptedException {
-        StringBuilder stringIn;
-        Data receive = new Data();
-
-        System.out.println("Listener started...");
-        while (true) {
-            while (in.available() == 0) {
-
-                Thread.sleep(10);
-            }
-            stringIn = new StringBuilder();
-            receive = new Data();
-
-            while (in.available() > 0) {
-                stringIn.append((char) in.readByte());
-            }
-
-            receive.readFromJson(new JSONObject(stringIn.toString()));
-
-            security.check(receive,connectionDB);
-
-            int action = receive.getAction();
-            if (action == ActionEnum.REGISTER.getI()) {
-                register(out, receive);
-            } else if (action == ActionEnum.LOGIN.getI()) {
-                login(out, receive);
-            } else if (action == ActionEnum.GET_LAST_CHATS.getI()) {
-                lastChats(out, receive);
-            } else if (action == ActionEnum.CREATE_CHAT.getI()) {
-                createChat(out, receive);
-            }else if(action == ActionEnum.ADD_TO_CHAT.getI()) {
-                addUserToGroup(out, receive);
-            }else if(action == ActionEnum.GET_LAST_MESSAGES.getI()){
-                lastMessages(out, receive);
-            }else if(action == ActionEnum.SEND_MESSAGE.getI()){
-                addMessage(out, receive);
-            }
-
-            Thread.sleep(10);
+    private String genID(){
+        Random r = new Random();
+        StringBuilder salt = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            salt.append((char)(r.nextInt(87)+36));
         }
+        return salt.toString();
     }
 
-    private void addMessage(DataOutputStream out, Data receive) throws IOException {
-        Message m = receive.getMessages()[0];
-        connectionDB.addMessage(m.getText(),m.getChat(),m.getSender(),m.getFlags());
-        out.write(new Data(ActionEnum.SUCCEED_MESSAGE_SEND.getI()).packToJson().toString().getBytes());
+    private void startThread(){
+        new Thread(()->{
+            while (true) {
+                for (Map.Entry<String, Client> client : clients.entrySet()) {
+                    try {
+                        client.getValue().listener();
+                        //System.out.printf("Listener called: %s.%n",client.getValue().getTokenName());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
-
-    private void lastMessages(DataOutputStream out, Data receive) throws IOException {
-        Data d = new Data(ActionEnum.SUCCEED_GET_LAST_MESSAGES.getI());
-        d.setMessages(connectionDB.getAllMessages((receive.getChats()[0].getChatID())));
-        out.write(d.packToJson().toString().getBytes());
-    }
-
-    private void addUserToGroup(DataOutputStream out, Data receive) throws IOException {
-        Chat c =receive.getChats()[0];
-        if(connectionDB.moveIntoChat(receive.getLogin().getName(),c.getChatID(),c.getFlags())){
-            out.write(new Data(ActionEnum.SUCCEED_ADD_TO_CHAT.getI()).packToJson().toString().getBytes());
-            System.out.printf("Invite from %s to %s.%n",receive.getToken(),receive.getLogin().getName());
-        }else{
-            out.write(new Data(ActionEnum.ERROR_ADD_TO_CHAT.getI()).packToJson().toString().getBytes());
-        }
-    }
-
-    private void createChat(DataOutputStream out, Data receive) throws IOException {
-        Chat c = receive.getChats()[0];
-        connectionDB.createChat(c.getCreatorID(), c.getTitle(), c.getFlags());
-        out.write(new Data(ActionEnum.SUCCEED_CREATE_CHAT.getI()).packToJson().toString().getBytes());
-    }
-
-    private void lastChats(DataOutputStream out, Data receive) throws IOException {
-        System.out.printf("Chat request from %s.%n", receive.getToken());
-        Chat[] c = connectionDB.getAllChats(Integer.parseInt(receive.getToken()));
-        out.write(new Data(ActionEnum.SUCCEED_GET_LAST_CHATS.getI(), c).packToJson().toString().getBytes());
-    }
-
-    private void login(DataOutputStream out, Data receive) throws IOException {
-        Login reg = receive.getLogin();
-        int usid = connectionDB.loginUser(reg.getName(), reg.getPassword());
-        if (usid != -1) {
-            out.write(new Data(ActionEnum.SUCCEED_LOGIN.getI(), null, null, String.valueOf(usid)/*Tokengen*/).packToJson().toString().getBytes());
-            System.out.printf("%s logged in.%n", reg.getName());
-        } else {
-            out.write(new Data(ActionEnum.ERROR_LOGIN.getI(), null, null, null).packToJson().toString().getBytes());
-        }
-    }
-
-    private void register(DataOutputStream out, Data receive) throws IOException {
-        Login reg = receive.getLogin();
-        if (connectionDB.registerUser(reg.getName(), reg.getPassword(), reg.getMale(), reg.getAge())) {
-
-            out.write(new Data(ActionEnum.SUCCEED_REGISTER.getI(), null, null, null /*Tokengen*/).packToJson().toString().getBytes());
-            System.out.printf("%s has registered.%n", reg.getName());
-        } else {
-            out.write(new Data(ActionEnum.ERROR_REGISTER.getI(), null, null, null).packToJson().toString().getBytes());
-        }
-    }
-
 
     public static void main(String[] args) {
         new Main();
